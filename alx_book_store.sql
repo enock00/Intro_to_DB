67a95faa806ecc4a33670be50550688d7ab5e0ec
Enock <enockzy54@gmail.com>
2025-07-04 09:12:24 -0700

    book store

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,16 +1,19 @@
--- alx_book_store.sql
+DROP TABLE IF EXISTS Order_Details;
+DROP TABLE IF EXISTS Orders;
+DROP TABLE IF EXISTS Customers;
+DROP TABLE IF EXISTS Books;
+DROP TABLE IF EXISTS Authors;
 
--- CREATE DATABASE
 CREATE DATABASE IF NOT EXISTS alx_book_store;
 USE alx_book_store;
 
--- CREATE TABLE: Authors
+-- Authors table
 CREATE TABLE Authors (
     author_id INT AUTO_INCREMENT PRIMARY KEY,
     author_name VARCHAR(215) NOT NULL
 );
 
--- CREATE TABLE: Books
+-- Books table
 CREATE TABLE Books (
     book_id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(130) NOT NULL,
@@ -20,28 +23,28 @@ CREATE TABLE Books (
     FOREIGN KEY (author_id) REFERENCES Authors(author_id)
 );
 
--- CREATE TABLE: Customers
+-- Customers table
 CREATE TABLE Customers (
     customer_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_name VARCHAR(215) NOT NULL,
-    email VARCHAR(215) UNIQUE NOT NULL,
+    email VARCHAR(215) NOT NULL,
     address TEXT
 );
 
--- CREATE TABLE: Orders
+-- Orders table
 CREATE TABLE Orders (
     order_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_id INT,
-    order_date DATE NOT NULL,
+    order_date DATE,
     FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
--- CREATE TABLE: Order_Details
+-- Order_Details table
 CREATE TABLE Order_Details (
     orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
     order_id INT,
     book_id INT,
-    quantity DOUBLE NOT NULL,
+    quantity DOUBLE,
     FOREIGN KEY (order_id) REFERENCES Orders(order_id),
     FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
