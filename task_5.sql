7732703159a6ed1ab51804d12928a0c1d0ab7b42
Enock <enockzy54@gmail.com>
2025-07-05 22:47:19 -0700

    a script that inserts a single row in the table customer

--- task_5.sql
+++ task_5.sql
@@ -1,7 +1,5 @@
 USE alx_book_store;
 
 INSERT INTO customer (customer_id, customer_name, email, address)
-VALUES
-    (1, 'Jane Doe', 'jane.doe@example.com', '123 Palm Street, Nairobi'),
-    (2, 'John Smith', 'john.smith@example.com', '456 River Road, Eldoret'),
-    (3, 'Alice Kiptoo', 'alice.kiptoo@example.com', '789 Mountain View, Kericho');
+VALUES 
+    (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
